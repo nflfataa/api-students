8e3b096ef5acd183ffd6a669272af9f9dea40426
nflfataa <naufalfataaa@gmail.com>
2026-04-20 15:36:19 +0700

    menambah jwt

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::apiResource('/students', StudentController::class);
+
+Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
